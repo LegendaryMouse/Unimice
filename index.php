71bbe0866194340fce52220a1dc1e6ddb88b6a3c
LMouse <qwertyuiop00110010@gmail.com>
2024-09-14 23:11:17 +0300

    Index

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <!-- Open Graph settings -->
+  <meta property="og:title" content="Приватные пиратские сервера Майнкрафт с модами без доната" />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://unimice.ru/" />
+  <meta property="og:image" content="/images/favicon.webp" />
+  <meta property="og:description"
+    content="👑 Наш проект Юнимайс постоянно создает новые приватные Майнкрафт сервера. Причем как сервера с модами и плагинами, так и ванильные сервера без модов" />
+  <meta property="og:site_name" content="Unimice" />
+
+  <!-- SEO settings -->
+  <meta name="keywords" content="👑 Наш проект Юнимайс постоянно создает новые приватные Майнкрафт сервера. Причем как сервера с модами и плагинами, так и ванильные сервера без модов" />
+  <title>Приватные пиратские сервера Майнкрафт с модами без доната</title>
+  <meta name="description" content="👑 UM постоянно создает новые приватные Майнкрафт сервера. Причем как сервера с модами и плагинами, так и ванильные сервера без модов" />
+
+  <!-- Canonical -->
+  <link rel=“canonical” href=“https://unimice.ru” />
+  
+  <!-- Encoding and Viewport settings -->
+  <!-- Cache settings -->
+  <!-- favicon settings -->
+  <!-- Stylesheets linking -->
+  <?php include 'modules/uniheader.php'; ?>
+
+</head>
+
+<body>
+  <main class="page-holder">
+    <!-- Header -->
+    <?php include 'modules/header.php'; ?>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <article><section id="main">
+      <div class="container">
+        <div class="third">
+          <h1>🔮 Приватные пиратские сервера Майнкрафт с модами без доната - Unimice</h1>
+
+          <a href="https://discord.gg/unimice"><div class="button rotator glow">Начать играть!</div></a>
+        </div>
+        <div class="third" itemscope itemtype="http://schema.org/ImageObject">
+          <img src="images/image.webp"
+            alt="Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Мышка"
+            itemprop="contentUrl" style="height: 300px" />
+          <meta itemprop="name" content="Unimice Minecraft Server Image" />
+          <meta itemprop="caption"
+            content="Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Мышка" />
+          <meta itemprop="description"
+            content="Изображение, показывающее Unimice - приватный сервер Minecraft с модами и плагинами без доната." />
+          <center>
+            <p class=f12>Мышка - маскот проекта Unimice</p>
+          </center>
+        </div>
+        <div class="third">
+          <h2>👑 Наш проект Юнимайс постоянно создает новые приватные Майнкрафт сервера. Причем как сервера с модами и
+            плагинами, так и ванильные сервера без модов</h2>
+          <h3>👾 Мы работаем над нашими серверами уже более 3-х лет и можем предложить вам лучшие сборки со стабильным
+            хостингом!</h3>
+        </div>
+      </div>
+    </section>
+    <!-- /Main -->
+
+    <!-- Advantages -->
+    <section id="advantages">
+        <h2 class="marginauto">Наши преимущества</h2>
+      <div class="container">
+        <div class="third glow rotator">
+          <p><strong>✌️ Без воды</strong></p>
+          <p>Удалили шаблонные преимущества типа "дружного сообщества" и "стабильного хостинга", которые есть у всех</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>🎮 Бесплатная проходка</strong></p>
+          <p>Вход на сервера бесплатен, что позволяет вам насладиться игрой без лишних затрат</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>❓  Сложнейший квест</strong></p>
+          <p>Прямо на этой страничке расположено множество секретов. Сможешь их разгадать?</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>❌ Без доната</strong></p>
+          <p>Играйте без необходимости донатить, все участники в равных условиях</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>🐭 Мышки-помощницы</strong></p>
+          <p>Получите помощь от наших виртуальных ассистентов, всегда готовых прийти на выручку</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>🎉 Гениальный заголовок</strong></p>
+          <p>Ну и ну, "🔮 Приватные пиратские сервера Майнкрафт с модами без доната - Unimice", как гордо!</p>
+        </div>
+
+        <div class="third glow rotator">
+          <p><strong>🚀 Быстрорастущее сообщество</strong></p>
+          <p>Набрали более 2000 участников Discord менее чем за год</p>
+        </div>
+      </div>
+    </section>
+    <!-- /Advantages -->
+    
+    <!-- Guide -->
+    <section id="guide">
+        <h2 class='marginauto'>Пошаговая инструкция как зайти на наши сервера</h2>
+      <div class="container" itemscope itemtype="http://schema.org/HowTo">
+        <div class="third glow rotator">
+          <p itemprop="name">Как зайти на сервера Unimice</p>
+          <p itemprop="description">Этот гайд поможет вам зайти на наши приватные сервера Minecraft с модами и плагинами</p>
+        </div>
+
+        <a href="https://unimice.ru/auth" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">1️⃣ Шаг 1: Авторизуйтесь на сайте</p>
+          <p itemprop="text">Создайте ваш профиль на сайте кликнув на этот шаг!</p>
+        </a>
+        
+        <a href="https://unimice.ru/form" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">2️⃣ Шаг 2: Заполните анкету</p>
+          <p itemprop="text">Заполните анкету нажав на этот шаг :)</p>
+        </a>
+        
+        <a href="https://unimice.ru/profile" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">3️⃣ Шаг 3: Ожидайте одобрения</p>
+          <p itemprop="text">Нажав на этот шаг вы попадете в личный кабинет, где можно отслеживать статус анкеты</p>
+        </a>
+        
+        <a href="https://discord.gg/unimice" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">4️⃣ Шаг 4: Скачайте лаунчер</p>
+          <p itemprop="text">После одобрения анкеты перейдите в наш Discord кликнув на этот шаг и скачайте лаунчер в канале #лаунчер</p>
+        </a>
+        
+        <a href="https://mousearth.ru" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">5️⃣ Шаг 5: Зарегистрируйтесь на сайте</p>
+          <p itemprop="text">Зарегистрируйтесь на сайте mousearth.ru</p>
+        </a>
+        
+        <div class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">6️⃣ Шаг 6: Зайдите в лаунчер</p>
+          <p itemprop="text">Зайдите в лаунчер с использованием аккаунта mousearth.ru</p>
+        </div>
+        
+        <div class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <p itemprop="name">7️⃣ Шаг 7: Успех</p>
+          <p itemprop="text">Теперь вы можете заходить на наши приватные сервера, такие как <a href="https://mousearth.ru" itemprop="url">Mouse Earth</a> или <a href="https://mousemoon.ru" itemprop="url">Mouse Moon</a></p>
+        </div>
+      </div>
+    </section>
+    <!-- /Guide -->
+
+
+
+    <!-- Popup -->
+    <?php //include 'modules/popup.php'; ?>
+    <!-- /Popup -->
+    
+    <!--Products -->
+    <section id="products">
+        <h2 class="marginauto">Наши продукты</h2>
+      <div class="container">
+        <!-- MouseEarth -->
+        <a href="https://mousearth.ru" aria-label="Сервер Mouse Earth - Minecraft 1.20.1 с модами Create и кастомными квестами">
+          <div class="third rotator" itemscope itemtype="http://schema.org/Product">
+
+            <meta itemprop="name" content="Mouse Earth">
+            <meta itemprop="description"
+              content="Mouse Earth - бесплатный приватный сервер с модами Create, Supplementaries, Simple swords и еще 150 модами и кастомными квестами">
+            <meta itemprop="image" content="/images/me.webp">
+
+            <a href="https://mousearth.ru" itemscope itemtype="http://schema.org/Service" aria-label="Перейти на Mouse Earth сервер">
+              <h3>🌎 Mouse Earth [Create Techno]</h3>
+              <meta itemprop="provider" content="Unimice">
+              <meta itemprop="image" content="/images/me.webp">
+              <p>Mouse Earth - бесплатный приватный сервер Майнкрафт Fabric 1.20.1 с модами Create, Supplementaries, Simple swords и еще 150 модами и кастомными квестами</p>
+              <p class="f12">"Добро пожаловать в Mouse Earth – ваш путь в мир стимпанка с модом Create! Присоединяйтесь к Мышиной Империи и станьте легендой среди игроков, создавая свою историю в этом мире шестеренок и механизмов. Ваше приключение начинается здесь"</p>
+            </a>
+            <?php include 'offer.php'; ?>
+          </div>
+        </a>
+        <!-- /MouseEarth -->
+
+        <!-- MouseMoon -->
+        <a href="https://mousemoon.ru" aria-label="Сервер Mouse Moon - Minecraft 1.20.1 с модами Ice and Fire и RPG">
+          <div class="third rotator" itemscope itemtype="http://schema.org/Product">
+
+            <meta itemprop="name" content="Mouse Moon">
+            <meta itemprop="description"
+              content="Mouse Moon - бесплатный приватный сервер с модами Ice and Fire, RPG, Simple swords и еще 150 модами и кастомными квестами">
+            <meta itemprop="image" content="/images/mm.webp">
+
+            <a href="https://mousemoon.ru" itemscope itemtype="http://schema.org/Service" aria-label="Перейти на Mouse Moon сервер">
+              <h3>🌕 Mouse Moon [Magic RPG]</h3>
+              <meta itemprop="provider" content="Unimice">
+              <meta itemprop="image" content="/images/mm.webp">
+              <p>Mouse Moon - бесплатный приватный сервер Майнкрафт Forge 1.20.1 с модами Ice and Fire, Origins, RPG, Simple swords и еще 150 модами и кастомными квестами</p>
+              <p class="f12">"Mouse Moon - твой лунный путь к бесконечным приключениям в мире Minecraft! Отправляйся в захватывающее путешествие под светом луны, где тайны и сказочная магия ждут тебя за каждым углом. Создавай свою уникальную историю вместе с нами"</p>
+            </a>
+            <?php include 'offer.php'; ?>
+          </div>
+        </a>
+        <!-- /MouseMoon -->
+        
+        <!-- Catangens -->
+        <a href="https://catangens.ru" aria-label="Сервер Catangens - Новый взгляд на мини-игры">
+          <div class="third rotator" itemscope itemtype="http://schema.org/Product">
+
+            <meta itemprop="name" content="Mouse Moon">
+            <meta itemprop="description"
+              content="Сервер Catangens - Новый взгляд на мини-игры">
+            <meta itemprop="image" content="/images/mm.webp">
+
+            <a href="https://catangens.ru" itemscope itemtype="http://schema.org/Service" aria-label="Перейти на Catangens сервер">
+              <h3>🐈 Catangens [Minigames]</h3>
+              <meta itemprop="provider" content="Unimice">
+              <meta itemprop="image" content="/images/mm.webp">
+              <p>Catangens — новый взгляд на мини-игры. Только интересные и весёлые мини-игры, музыка, поддержка голосового чата!</p>
+
+            </a>
+            <?php include 'offer.php'; ?>
+          </div>
+        </a>
+        <!-- /Catangens -->
+
+        <!-- MouseSun -->
+        <a href="#" aria-label="Сервер Mouse Sun - Лучшее ванильное выживание Minecraft 1.21">
+          <div class="third rotator" itemscope itemtype="http://schema.org/Product">
+
+            <meta itemprop="name" content="Mouse Sun">
+            <meta itemprop="description"
+              content="Mouse Sun - лучший ванильный сервер Майнкрафт 1.21 без привата и без доната. Топ ванильное выживание без привата, доната и с отборными плагинами на оптимизацию">
+            <meta itemprop="image" content="/images/mm.webp">
+
+            <a href="#" itemscope itemtype="http://schema.org/Service" aria-label="Перейти на Mouse Sun сервер">
+              <h3>☀️ Mouse Sun [Vanilla]</h3>
+              <meta itemprop="provider" content="Unimice">
+              <meta itemprop="image" content="/images/mm.webp">
+              <p>Mouse Sun - лучший ванильный пиратский сервер Майнкрафт Purpur 1.21 без привата и без доната. Топ ванильное выживание без привата, доната и с отборными плагинами на оптимизацию</p>
+              <p class="f12">"Mouse Sun - твой солнечный путь к незабываемым приключениям в мире Minecraft! Открой для себя захватывающее выживание под светом солнца. Каждый день здесь приносит новые испытания и возможности. Создавай свою уникальную историю на лучшем ванильном сервере вместе с нами"</p>
+            </a>
+            <?php include 'offer.php'; ?>
+          </div>
+        </a>
+        <!-- /MouseSun -->
+
+        <!-- Unimice -->
+        <a href="#" aria-label="Лаунчер Unimice для пиратских серверов Minecraft">
+          <div class="third rotator" itemscope itemtype="http://schema.org/Product">
+
+            <meta itemprop="name" content="Unimice лаунчер">
+            <meta itemprop="description"
+              content="Лаунчер позволяет зайти на сервер с пиратской версии, без лицензии Minecraft. Чтобы создать проект без грифа, но с бесплатной проходкой мы разработали многоступенчатую систему верификации игроков.">
+            <meta itemprop="image" content="/images/logo.webp">
+
+            <a href="#" itemscope itemtype="http://schema.org/Service" aria-label="Перейти к скачиванию лаунчера Unimice">
+              <h3>✨ Unimice лаунчер</h3>
+              <p>Лаунчер позволяет зайти на сервер с пиратской версии, без лицензии Minecraft. Чтобы создать проект без грифа, но с бесплатной проходкой мы разработали многоступенчатую систему верификации игроков</p>
+              <p class="f12">"Лаунчер созданный на основе Gravit Launcher, но с несколькими кастомными модификациями обеспечивает удобство, быстрый поиск и просмотр статуса серверов. В тоже время он обеспечивает полную безопасность игроков и серверов"</p>
+            </a>
+            <?php include 'offer.php'; ?>
+          </div>
+        </a>
+        <!-- /Unimice -->
+        
+        
+
+        <!-- Info -->
+        <div class="third">
+          <h3>🔧Техническая информация</h3>
+          <p id="average-tps">📊 Средний TPS: 19.55</p>
+          <p id="online-count">👥 Общий онлайн: 30</p>
+          <p id="truthfulness">🎭 Правдивость статистики: 05.00%</p>
+        </div>
+        <script>
+          function getRandomNumber(min, max) {
+            return Math.random() * (max - min) + min;
+          }
+
+          function updateTPS() {
+            const tpsElement = document.getElementById('average-tps');
+            const randomTPS = (getRandomNumber(19.00, 20.10)).toFixed(2);
+            tpsElement.textContent = `📊 Средний TPS: ${randomTPS}`;
+
+            const truthfulnessElement = document.getElementById('truthfulness');
+            const truthfulness = 20 - parseFloat(randomTPS);
+            truthfulnessElement.textContent = `🎭 Правдивость статистики: ${truthfulness.toFixed(2)}%`;
+          }
+
+          let currentOnline = 30; // Инициализация текущего онлайна
+
+          function updateOnlineCount() {
+            const onlineElement = document.getElementById('online-count');
+            const change = Math.floor(getRandomNumber(-3, 4));
+            currentOnline = Math.max(0, currentOnline + change);
+            onlineElement.textContent = `👥 Общий онлайн: ${currentOnline}`;
+          }
+
+          setInterval(updateTPS, 500);
+          setInterval(updateOnlineCount, 15000);
+        </script>
+        <!-- /Info -->
+
+      </div>
+    </section>
+    <!-- /Products -->
+
+    <!-- Easter -->
+    <?php include 'modules/easter.php'; ?>
+    <!-- /Easter -->
+    
+  </article></main>
+  <!-- Footer -->
+  <?php include 'modules/footer.php'; ?>
+  <!-- /Footer -->
+
+</body>
+
+</html>
