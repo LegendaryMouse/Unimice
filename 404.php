81b2fe820a464a0aa0b1d6949d8ac37933cafaeb
LMouse <qwertyuiop00110010@gmail.com>
2024-09-15 13:36:33 +0300

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+<head>
+    <?php header("HTTP/1.0 404 Not Found"); ?>
+    <title>Ошибка 404 - нету у нас такой страницы!</title>
+    <meta name="description" content="Ошибка 404 - нету у нас такой страницы!">
+
+    <?php include "modules/uniheader.php"; ?>
+</head>
+<body>
+    <!-- Header -->
+    <?php include 'modules/header.php'; ?>
+    <!-- /Header -->
+    <div class="container">
+        <div class="double"><h1>Ошибка 404 - нету у нас такой страницы!</h1></div>
+        <div class="third"><h2><a href='https://unimice.ru'>[Вернуться на главную]</a></h2></div>
+    </div>
+    <?php include 'modules/footer.php'; ?>
+</body>
+</html>
