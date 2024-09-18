be675620c679d8d1b9eb3e7d1a8b7af247f72fdc
LMouse <qwertyuiop00110010@gmail.com>
2024-09-18 08:30:31 +0300

    Create staff.php

---
+++ staff.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <!-- Open Graph settings -->
+  <meta property="og:title" content="Лучшие приватные пиратские сервера Майнкрафт с модами без доната - Персонал" />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://unimice.ru/" />
+  <meta property="og:image" content="/images/favicon.webp" />
+  <meta property="og:description"
+    content="Лучшие приватные пиратские сервера Майнкрафт с модами без доната - Персонал" />
+  <meta property="og:site_name" content="Unimice" />
+
+  <!-- SEO settings -->
+  <meta name="keywords" content="Лучшие приватные пиратские сервера Майнкрафт с модами без доната - Персонал" />
+  <title>Лучшие приватные пиратские сервера Майнкрафт с модами без доната - Персонал</title>
+  <meta name="description" content="Лучшие приватные пиратские сервера Майнкрафт с модами без доната - Персонал" />
+
+  <!-- Canonical -->
+  <link rel=“canonical” href=“https://unimice.ru/staff” />
+
+  <!-- Encoding and Viewport settings -->
+  <!-- Cache settings -->
+  <!-- favicon settings -->
+  <!-- Stylesheets linking -->
+  <?php include 'modules/uniheader.php'; ?>
+
+</head>
+
+<body>
+  <div class="page-holder">
+    <!-- Header -->
+    <?php include 'modules/header.php'; ?>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <section id="main">
+      <div class="container">
+        <div class="third">
+          <h1>🔮 Персонал проекта Unimice</h1>
+
+          <a href="https://discord.gg/unimice"><div class="button rotator glow">Начать играть!</div></a>
+        </div>
+        <div class="third" itemscope itemtype="http://schema.org/ImageObject">
+          <img src="images/image.webp"
+            alt="Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Мышка"
+            itemprop="contentUrl" style="height: 300px" />
+          <meta itemprop="name" content="Unimice Minecraft Server Image" />
+          <meta itemprop="caption"
+            content="Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Мышка" />
+          <meta itemprop="description"
+            content="Изображение, показывающее Unimice - приватный сервер Minecraft с модами и плагинами без доната." />
+          <center>
+            <p class=f12>Мышка - маскот проекта Unimice</p>
+          </center>
+        </div>
+        <div class="third">
+          <h2>💻 Чтобы создавать качественные сервера нужны крутые разработчики, а чтобы поддерживать на них уютную атмосферу нужны крутые модераторы. У нас есть и те, и те!</h2>
+          <h3>👑 Только благодаря персоналу проект Unimice стал тем что он есть, и становится всё лучше и лучше</h3>
+        </div>
+      </div>
+    </section>
+    <!-- /Main -->
+
+    <!-- Personal -->
+    <section id="personal">
+      <center>
+        <h2>Персонал Unimice</h2>
+      </center>
+      <center>
+        <h3>Администрация</h3>
+      </center>
+      <div class="container">
+        <a href="https://unimice.ru/personal/legendarymouse">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">👑 mouse</span></p>
+            <img src="images/lmouse.webp" class="image" alt="legendarymouse" itemprop="image">
+            <p><span itemprop="jobTitle">Маскот</span></p>
+            <link itemprop="url" href="https://legacy.curseforge.com/members/lmouse">
+            <link itemprop="sameAs" href="https://legacy.curseforge.com/members/lmouse">
+            <meta itemprop="worksFor" content="Unimice">
+          </div>
+        </a>
+
+        <a href="https://unimice.ru/personal/hezika">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">🔧 hezika</span></p>
+            <img src="images/hezy.webp" class="image" alt="hezika" itemprop="image">
+            <p><span itemprop="jobTitle">Техадмин</span></p>
+            <link itemprop="url" href="https://modrinth.com/user/coderbot">
+            <link itemprop="sameAs" href="https://modrinth.com/user/coderbot">
+            <meta itemprop="worksFor" content="Unimice">
+          </div>
+        </a>
+
+        <a href="personal/pyth.php">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">💻 pyth</span></p>
+            <img src="images/pyth.webp" class="image" alt="pyth_official" itemprop="image">
+            <p><span itemprop="jobTitle">Разработчик</span></p>
+            <link itemprop="url" href="https://modrinth.com/user/modmuss50">
+            <link itemprop="sameAs" href="https://modrinth.com/user/modmuss50">
+            <meta itemprop="worksFor" content="Unimice">
+          </div>
+        </a>
+
+        <a href="https://unimice.ru/#">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">🤝 advisor</span></p>
+            <img src="images/advisor.webp" class="image" alt="advisor" itemprop="image">
+            <p><span itemprop="jobTitle">Руководитель</span></p>
+            <link itemprop="url" href="https://legacy.curseforge.com/members/lmouse">
+            <link itemprop="sameAs" href="https://legacy.curseforge.com/members/lmouse">
+            <meta itemprop="worksFor" content="Unimice">
+          </div>
+        </a>
+      </div>
+
+      <center>
+        <h3>Модерация</h3>
+      </center>
+      <div class="container">
+        <a href="https://unimice.ru/personal/fedorsmurf">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">✍️ fedorsmurf</span></p>
+            <img src="images/fedor.webp" class="image" alt="fedorsmurf">
+            <p><span itemprop="jobTitle">Писатель</span></p>
+          </div>
+        </a>
+
+        <a href="https://unimice.ru/personal/nogamemode">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">👮 nogamemode</span></p>
+            <img src="images/nogame.webp" class="image" alt="NoGamemode">
+            <p><span itemprop="jobTitle">Модератор</span></p>
+          </div>
+        </a>
+
+        <a href="https://unimice.ru/personal/lohodei">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">👮 lohodei</span></p>
+            <img src="images/lohodei.webp" class="image" alt="Lohodei">
+            <p><span itemprop="jobTitle">Модератор</span></p>
+          </div>
+        </a>
+
+        <a href="https://unimice.ru/personal/tarakanozavr">
+          <div class="text rotator glow" itemscope itemtype="http://schema.org/Person">
+            <p><span itemprop="name">🎉 tarakan</span></p>
+            <img src="images/tarakan.webp" class="image" alt="tarakanozavr">
+            <p><span itemprop="jobTitle">Ивентер</span></p>
+          </div>
+        </a>
+      </div>
+    </section>
+    <!-- /Personal -->
+
+  </div>
+  <!-- Footer -->
+  <?php include 'modules/footer.php'; ?>
+  <!-- /Footer -->
+
+</body>
+
+</html>
