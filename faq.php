d69a2b564f140679e4fbcbca8128b1ce61f03ce1
LMouse <qwertyuiop00110010@gmail.com>
2024-09-19 13:49:10 +0300

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <!-- Open Graph settings -->
+  <meta property="og:title" content="Лучшие приватные сервера Майнкрафт с модами без доната, без лицензии - Частые вопросы" />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://unimice.ru/faq" />
+  <meta property="og:image" content="/images/favicon.webp" />
+  <meta property="og:description"
+    content="Лучшие приватные сервера Майнкрафт с модами без доната, без лицензии - Частые вопросы" />
+  <meta property="og:site_name" content="Unimice" />
+
+  <!-- SEO settings -->
+  <meta name="keywords" content="Лучшие приватные пиратские сервера Майнкрафт с модами без доната, без лицензии" />
+  <title>Лучшие приватные сервера Майнкрафт с модами без доната, без лицензии - Частые вопросы</title>
+  <meta name="description" content="Лучшие приватные сервера Майнкрафт с модами без доната, без лицензии - Частые вопросы" />
+
+  <!-- Canonical -->
+  <link rel=“canonical” href=“https://unimice.ru/faq” />
+
+  <!-- Encoding and Viewport settings -->
+  <!-- Cache settings -->
+  <!-- favicon settings -->
+  <!-- Stylesheets linking -->
+  <?php include 'modules/uniheader.php'; ?>
+
+</head>
+
+<body>
+  <div class="page-holder">
+    <!-- Header -->
+    <?php include 'modules/header.php'; ?>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <section id="main">
+      <div class="container">
+        <div class="third">
+          <h1>🔮 Частые вопросы - Unimice</h1>
+
+          <div class="button rotator glow"><a href="https://discord.gg/unimice">Остались вопросы!</a></div>
+        </div>
+        <div class="third" itemscope itemtype="http://schema.org/ImageObject">
+          <img src="https://unimice.ru/images/faqmouse.webp"
+            alt="Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Частые вопросы - Старшая Мышка"
+            itemprop="contentUrl" style="height: 300px" />
+          <meta itemprop="name" content="Unimice Minecraft Server Image" />
+          <meta itemprop="caption"
+            content="Частые вопросы - Старшая Мышка" />
+          <meta itemprop="description"
+            content="Изображение, показывающее Приватные Майнкрафт сервера с модами и плагинами без доната - Unimice - Унимайс - Частые вопросы - Старшая Мышка" />
+          <center>
+            <p class=f12>Старшая Мышка - помогает игрокам UniMice</p>
+          </center>
+        </div>
+        <div class="third">
+          <h2>❓ Если у вас возникли вопросы по нашему проекту - на этой странице вы вероятнее всего найдете ответы на них</h2>
+          <h3>❗️ Здесь собраны все часто задаваемые вопросы, а также ответы на них</h3>
+        </div>
+      </div>
+    </section>
+    <!-- /Main -->
+
+    <!-- FAQ -->
+    <section id="faq" itemscope itemtype="https://schema.org/FAQPage">
+      <center>
+        <h2>Часто задаваемые вопросы</h2>
+      </center>
+      <div class="container">
+        
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🎯・В чем смысл жизни?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">42</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <a href="https://unimice.ru/questions/how-to-join"><h2 itemprop="name">🔗・Как зайти на сервер?</h2></a>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Подробная инструкция как зайти на наши сервера расположена ниже, <a href="https://unimice.ru/faq/#guide">ознакомьтесь здесь</a>.</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">⏱️・Как долго проверяют анкеты?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Проверка анкеты займет не больше 24 часов. Однако, по различным обстоятельствам рассмотрение могут приостановить, подробнее об этом можете узнать в Discord канале #верификация</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🕹️・На каких версиях Майнкрафт работают ваши сервера?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Сервера Mouse Earth и Mouse Moon работают на версии 1.20. Сервер Mouse Sun работает на версии 1.21.</p>
+          </div>
+        </div>
+        
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">💰・Действительно ли все сервера без доната?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Все Майнкрафт сервера которые создает наш проект - абсолютно бесплатны! Без платной проходки, без доната, мы делаем всё, чтобы наши игроки могли наслаждаться качественными сборками совершенно свободно. Если вы хотите поддержать проект - вы можете оформить подписку на <a href="https://boosty.to/legendary_mouse">Наш Boosty</a> и пожертвовать любую, приемлемую для вас сумму</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🚪・Можно ли зайти с пиратки / без лицензии?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Да, конечно! Мы создаем лучшие пиратские сервера, поэтому вы можете зайти без лицензии</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🛠️・Нужно ли скачивать лаунчер и моды чтобы зайти на сервер?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Да, мы создаем сервера с модами и лаунчером, поэтому чтобы зайти к нам необходимо установить наш лаунчер, который в свою очередь автоматически установит моды. Доступ к скачиванию не займет много времени, и будет доступен сразу после прохождения верификации</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🗺️・Как вы создаете сервера с квестами?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Мы используем модификацию FTBquests, чтобы создавать и настраивать целые деревья квестов и достижений</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🛡️・Нет ли проблем с гриферами из-за бесплатной проходки?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Абсолютно нет! У нас есть многоступенчатая система верификации и проверки, которая позволяет обезопасить сервера от гриферов</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🤝・Почему у вас все сервера без грифа?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Мы считаем что все игроки должны сосуществовать мирно, и делиться своим творчеством с другими, что невозможно на серверах с грифом и анархией</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🚪・Когда следующий вайп?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Вайпы оглашаются за месяц, если нет новости о вайпе, значит он нескоро</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">⏳・Сколько времени рассматривают анкету?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Все накопившиеся анкеты рассматриваются каждый день ровно в 19:00 МСК. Если вы подали анкету позже - она будет рассмотрена на следующий день.</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">🌍・Чем отличаются Mouse Earth и Mouse Moon?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">Unimice - проект создающий сервера.
+            Mouse Earth (МЕ) - сервер с Create.
+            Mouse Moon (ММ) - сервер с магией.</p>
+          </div>
+        </div>
+
+        <div class="third rotator" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+          <h2 itemprop="name">📩・У меня остались вопросы, к кому обратиться?</h2>
+          <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+            <p itemprop="text">По оставшимся вопросам пишите @mouse_advisor</p>
+          </div>
+        </div>
+        
+      </div>
+    </section>
+    <!-- /FAQ -->
+
+    <!-- Guide -->
+    <section id="guide">
+      <center>
+        <h2>Пошаговая инструкция как зайти на наши сервера</h2>
+      </center>
+      <div class="container" itemscope itemtype="http://schema.org/HowTo">
+        <div class="third glow rotator">
+          <h2 itemprop="name">Как зайти на сервера Unimice</h2>
+          <p itemprop="description">Этот гайд поможет вам зайти на наши приватные сервера Minecraft с модами и
+            плагинами.</p>
+        </div>
+
+        <a href="https://unimice.ru/auth" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">1️⃣ Шаг 1: Авторизуйтесь на сайте</h2>
+          <p itemprop="text">Создайте ваш профиль на сайте кликнув на этот шаг!</p>
+        </a>
+
+        <a href="https://unimice.ru/form" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">2️⃣ Шаг 2: Заполните анкету</h2>
+          <p itemprop="text">Заполните анкету нажав на этот шаг :)</p>
+        </a>
+
+        <a href="https://discord.gg/unimice" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">3️⃣ Шаг 3: Скачайте лаунчер</h2>
+          <p itemprop="text">Перейдите в наш Discord кликнув на этот шаг и скачайте лаунчер в канале #лаунчер</p>
+        </a>
+
+        <a href="https://mousearth.ru" class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">4️⃣ Шаг 4: Зарегистрируйтесь на сайте</h2>
+          <p itemprop="text">Зарегистрируйтесь на сайте mousearth.ru.
+          </p>
+        </a>
+
+        <div class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">5️⃣ Шаг 5: Зайдите в лаунчер</h2>
+          <p itemprop="text">Зайдите в лаунчер с использованием аккаунта mousearth.ru.</p>
+        </div>
+
+        <div class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">6️⃣ Шаг 6: Успех</h2>
+          <p itemprop="text">Теперь вы можете заходить на любые сервера, такие как <a href="https://mousearth.ru"
+              itemprop="url">Mouse Earth</a> или <a href="https://mousemoon.ru" itemprop="url">Mouse Moon</a>.</p>
+        </div>
+
+        <div class="third rotator" itemprop="step" itemscope itemtype="http://schema.org/HowToStep">
+          <h2 itemprop="name">7️⃣ Шаг 7: Жичжжу</h2>
+          <p itemprop="text">24-6-9-1-18-30 19-14-6-27-6-15-10-6 15-1 26-1-4</p>
+        </div>
+      </div>
+    </section>
+    <!-- /Guide -->
+    
+  </div>
+  <!-- Footer -->
+  <?php include 'modules/footer.php'; ?>
+  <!-- /Footer -->
+
+</body>
+
+</html>
