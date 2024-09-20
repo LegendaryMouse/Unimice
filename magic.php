69ed116378ddee5b4186bfed6df0c27edf6f4acc
LMouse <qwertyuiop00110010@gmail.com>
2024-09-20 14:38:01 +0300

    Added supercool magic channel

---
+++ magic.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <!-- Open Graph settings -->
+  <meta property="og:title" content="Волшебный способ решения всех ваших проблем" />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://unimice.ru/" />
+  <meta property="og:image" content="/images/favicon.webp" />
+  <meta property="og:description"
+    content="Перейдя по этой (невероятно!) волшебной ссылке вы попадете на магическую страницу где вам покажут как избавиться от всех забот и получить все земные блага и любовь администрации. Лучше и не придумать!" />
+  <meta property="og:site_name" content="Unimice" />
+
+  <!-- SEO settings -->
+  <meta name="keywords" content="Волшебный способ решения всех ваших проблем" />
+  <title>Волшебный способ решения всех ваших проблем</title>
+  <meta name="description" content="Перейдя по этой (невероятно!) волшебной ссылке вы попадете на магическую страницу где вам покажут как избавиться от всех забот и получить все земные блага и любовь администрации. Лучше и не придумать!" />
+
+
+  <!-- Encoding and Viewport settings -->
+  <!-- Cache settings -->
+  <!-- favicon settings -->
+  <!-- Stylesheets linking -->
+  <?php include 'https://unimice.ru/modules/uniheader.php'; ?>
+</head>
+
+<body>
+    <?php
+    echo '<script type="text/javascript">
+            window.location.href = "https://boosty.to/legendary_mouse";
+          </script>';
+    exit();
+    ?>
+</body>
